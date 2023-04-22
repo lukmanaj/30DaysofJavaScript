341d0db0d06c78aa6ec9dc18409869290369050c
Lukman Aliyu <aliyulukman94@gmail.com>
2023-04-22 14:36:05 +0100

    update

--- day_23_Event_listeners/project_2/scripts/main.js
+++ day_23_Event_listeners/project_2/scripts/main.js
@@ -1,37 +1,29 @@
 const style = document.createElement('style');
 style.innerHTML = `
-
 body{
-    font-family: Arial, Helvetica, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
 }
 .key{
-    padding:4px;
-    margin:auto 0;
-    font-size: large;
-    width:280px;
-    border:2px solid grey;
-    margin-top: 100px;
-    margin-bottom: 100px;
-    margin-right: auto;
-    margin-left: auto;
-    text-align: center;
+  padding: 4px;
+  margin: 100px auto;
+  font-size: large;
+  max-width: 280px;
+  border: 2px solid grey;
+  text-align: center;
 }
 .key-code{
-    padding:4px;
-    font-size: large;
-    height:80px;
-    width: 60px;
-    border:1px solid grey;
-    margin-top: 100px;
-    margin-bottom: 100px;
-    margin-right: auto;
-    margin-left: auto;
-    text-align: center;
+  padding: 4px;
+  font-size: large;
+  height: 80px;
+  width: 60px;
+  border: 1px solid grey;
+  margin: 100px auto;
+  text-align: center;
 }
 .green{
-    color:rgb(139, 236, 139)
+  color: rgb(139, 236, 139);
 }
-`
+`;
 document.body.appendChild(style);
 
 const body = document.querySelector("body");
